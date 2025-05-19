eacbb0ccd519181020d12ec2091c9936a3b65cbf
Anhelina <parshinaangel@gmail.com>
2025-05-19 21:37:12 +0300

    add js meny-open

--- src/js/modal.js
+++ src/js/modal.js
@@ -69,3 +69,21 @@
     document.body.classList.toggle('no-scroll');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-meny-open]'),
+
+    closeModalBtn: document.querySelector('[data-meny-close]'),
+
+    modal: document.querySelector('[data-meny]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
