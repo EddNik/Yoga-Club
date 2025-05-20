ccc60a738f85c6d8c9286e205af2944a11c91242
Anhelina <parshinaangel@gmail.com>
2025-05-20 12:47:11 +0300

    add fill for svg in prising.css , add id in about-us.html testimonials.html and yoga-classes.html. fiix mistake in modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -95,7 +95,6 @@
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
